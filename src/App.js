57883eccc58aa8453bc84b2f5d461da4429a7dcc
Anastasia Fomina <anastasia.o.fomina@gmail.com>
2019-01-11 16:27:50 +0300

    add init + base logic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './styles/App.css'
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props) 
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+  
+  getData = () => {
+    fetch('http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')
+    .then((result) => {
+      return result.json()
+    })
+    .then((data) => {
+      this.setState({
+        users: data
+      })
+    })
+  }
+  
+ renderRow = (item) => {
+  const { id, firstName, lastName, email, phone } = item
+
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{firstName}</td>
+      <td>{lastName}</td>
+      <td>{email}</td>
+      <td>{phone}</td>
+    </tr>
+   )
+ }
+
   render() {
+    const rows = this.state.users.map(this.renderRow)
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+       <table>
+        <tbody>
+          <tr>
+            <th>id</th>
+            <th>firstName</th>
+            <th>lastName</th>
+            <th>email</th>
+            <th>phone</th>
+          </tr>
+          {rows}
+        </tbody>
+       </table>
       </div>
-    );
+    )
   }
 }
 
